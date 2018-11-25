59c46af792df26b75f9c87349edd1c1294d56205
Joapfel <jojokr94@gmail.com>
2018-11-26 00:46:05 +0100

    added Quicksort

--- Sortings.go
+++ Sortings.go
@@ -8,17 +8,19 @@ func main() {
 	fmt.Println("Hello World!")
 
 	L := []int{5, 8, 4, 345, 8, 9, 2, 1, 3, 4, 6, 9, 4, 3, 3, 6, 7, 3, 4, 5}
-	fmt.Println(L)
-
-	quicksort(L)
+	fmt.Println("Unsorted List: ",L)
+	fmt.Println("Sorted List", quicksort(L))
 }
 
-func quicksort(L []int) {
+func quicksort(L []int) []int{
+	if len(L) == 1{return []int{L[0]}}
+	if len(L) == 0{return []int{}}
+
 	pivot := L[0]
 	var left []int
 	var right []int
 
-	for _, el := range L {
+	for _, el := range L[1:] {
 		if el <= pivot {
 			left = append(left, el)
 		} else {
@@ -26,8 +28,7 @@ func quicksort(L []int) {
 		}
 	}
 
-	fmt.Println("Pivot: ", pivot)
-	fmt.Println("Left: ", left)
-	fmt.Println("Right: ", right)
-	fmt.Println()
+	joined := append(quicksort(left), pivot)
+	joined = append(joined, quicksort(right)...)
+	return joined
 }
